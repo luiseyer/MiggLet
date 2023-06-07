8f2eb6233810b8dd37f4b9e34cb7907c0d6c9b95
Luis Fernando Reyes <luisfernandorp2001@gmail.com>
2023-06-07 00:55:24 -0400

    prototipo de estadísticas

--- app/src/pages/Dashboard.jsx
+++ app/src/pages/Dashboard.jsx
@@ -4,9 +4,10 @@ import { PageContainer, Section, NavigationMenu, PatientList } from '@components
 import data from '@helpers/data'
 
 const Item = styled(Paper)(({ theme }) => ({
-  padding: 4,
+  padding: '1rem',
   textAlign: 'center',
-  color: theme.palette.light.main
+  color: theme.palette.light.main,
+  flex: '1 1 250px'
 }))
 
 const DashboardPage = () => {
@@ -16,16 +17,16 @@ const DashboardPage = () => {
     <PageContainer>
       <NavigationMenu />
       <Section color='light.main'>
-        <Box sx={{ display: 'grid', gridTemplateColumns: '1fr', gap: 2, mb: 4 }}>
-          <Item elevation={3} sx={{ backgroundColor: 'primary.main' }}>
+        <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 2, mb: 4 }}>
+          <Item elevation={3} sx={{ bgcolor: 'primary.main' }}>
             <Typography variant='h3'>15.454</Typography>
             Total de Pacientes
           </Item>
-          <Item elevation={3} sx={{ backgroundColor: 'secondary.main' }}>
+          <Item elevation={3} sx={{ bgcolor: 'secondary.main' }}>
             <Typography variant='h3'>454</Typography>
             Pacientes Diarios
           </Item>
-          <Item elevation={3} sx={{ backgroundColor: 'tertiary.main' }}>
+          <Item elevation={3} sx={{ bgcolor: 'tertiary.main' }}>
             <Typography variant='h3'>3.000</Typography>
             Otra Cosa
           </Item>
